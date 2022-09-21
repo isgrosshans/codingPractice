4ad7d24ca0d0d48c57b0b07a74c418b250d3c455
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-09-21 20:51:16 +0200

    Time: 2 ms (91.12%), Space: 42.1 MB (50.96%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         char[] stack = new char[s.length()];
         int top = -1; //empty
+        if(s.length()%2!=0) return false;
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)=='('
               || s.charAt(i)=='['
