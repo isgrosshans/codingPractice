c8057803d36a753e2d40e05409d7cc0e2a19ea5c
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-09-21 15:55:18 +0200

    Time: 5 ms (75.13%), Space: 43.6 MB (67.38%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alphabetCounter = new int[26];
+        for(int i=0; i<s.length(); i++){
+            alphabetCounter[s.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<t.length(); i++){
+            alphabetCounter[t.charAt(i) - 'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(alphabetCounter[i]!=0) return false;
+        }
+        return true;
+    }
+}
