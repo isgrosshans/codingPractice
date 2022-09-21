32a2bcc436316b45b1c119a831c173a407f8c7aa
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-09-21 16:03:18 +0200

    Time: 5 ms (75.13%), Space: 43.2 MB (82.56%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
+
         int[] alphabetCounter = new int[26];
         for(int i=0; i<s.length(); i++){
             alphabetCounter[s.charAt(i) - 'a']++;
