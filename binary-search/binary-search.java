9ed7869990a9bc2d6134172ada269d10127506d0
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-09-23 12:16:09 +0200

    Time: 0 ms, Memory: 54.2 MB - LeetHub

---
+++ binary-search/binary-search.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid;
+        
+        while(left<=right){
+            mid = (left+right)/2;
+            if(target == nums[mid]) return mid;
+            if(target < nums[mid]) right = mid-1;
+            if(target > nums[mid]) left = mid+1;
+        }
+        return -1;
+    }
+}
