85757c22bb167ea9d97b5352d6cd47de2b5bff9e
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-09-28 16:40:47 +0200

    Time: 3 ms (9.76%), Space: 43.3 MB (73.87%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -34,9 +34,7 @@ class Solution {
             for(TreeNode node : currentLevel){
                 if(node!=null){
                     levelVals.add(node.val);
-                    // if(node.left!=null)
                         nextLevel.add(node.left);
-                    // if(node.right!=null)
                         nextLevel.add(node.right);
                 }
             }
