0bec4ed89068606ac2fabc2fe9ed647a06c49a77
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-10-01 11:04:25 +0200

    Time: 5 ms (61.23%), Space: 57.2 MB (62.92%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,12 +1,11 @@
 class Solution {
-    Set<Integer> visited = new HashSet();
     
     public int numIslands(char[][] grid) {
         int islandCounter = 0;
         
         for(int row=0; row < grid.length; row++){
             for(int col=0; col < grid[0].length; col++){
-                if(grid[row][col]=='1' && !(visited.contains(cellId(grid,row,col)))){
+                if(grid[row][col]=='1'){
                     exploreIsland(grid, row, col);
                     islandCounter++;
                 }
@@ -17,11 +16,10 @@ class Solution {
     }
     
     void exploreIsland(char[][] grid, int row, int col){
-        if(visited.contains(cellId(grid,row,col))) return;
         if(row<0 || col<0 || row>=grid.length || col>=grid[0].length) return;
         
         if(grid[row][col]=='1'){
-            visited.add(cellId(grid,row,col));
+            grid[row][col]='2';
             exploreIsland(grid,row+1,col);
             exploreIsland(grid,row-1,col);
             exploreIsland(grid,row,col+1);
