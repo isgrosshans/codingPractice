9a828e97643958eecfb26b3ed346847869f20ac6
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-10-01 11:08:32 +0200

    Time: 6 ms (46.19%), Space: 57.5 MB (43.07%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -20,14 +20,10 @@ class Solution {
         
         if(grid[row][col]=='1'){
             grid[row][col]='2';
-            exploreIsland(grid,row+1,col);
-            exploreIsland(grid,row-1,col);
-            exploreIsland(grid,row,col+1);
-            exploreIsland(grid,row,col-1);
+            if(row+1<grid.length)       exploreIsland(grid,row+1,col);
+            if(row>0)                   exploreIsland(grid,row-1,col);
+            if(col+1<grid[0].length)    exploreIsland(grid,row,col+1);
+            if(col>0)                   exploreIsland(grid,row,col-1);
         }
     }   
-    
-    int cellId(char[][] grid, int row, int col){
-        return row * grid[0].length + col;
-    }
 }
