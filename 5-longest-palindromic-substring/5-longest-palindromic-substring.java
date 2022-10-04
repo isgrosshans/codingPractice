36ec282f26b40db22dec98e59c970f49baf40f30
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-10-04 16:12:33 +0200

    Time: 42 ms (54.24%), Space: 42.8 MB (66.90%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,42 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s.length()<2) return s;
+        if(s==null || s.length()<2) return s;
         
-        int leftPivot;
-        int rightPivot;
-        String answer=s.charAt(0)+"";
-        
+        int start=0, end=0;
         for(int i = 0; i + 1 < s.length(); i++){
-            
-            if(i+2<s.length() && s.charAt(i)==s.charAt(i+2)){
-                
-                leftPivot=i;
-                rightPivot=i+2;
-                String tempPalindrome=""+s.charAt(i+1);
-                while(leftPivot>=0 && rightPivot<s.length() &&
-                    s.charAt(leftPivot)==s.charAt(rightPivot)){
-                    tempPalindrome = s.charAt(leftPivot--)
-                        + tempPalindrome
-                        + s.charAt(rightPivot++);
-                }
-                if(tempPalindrome.length()>answer.length())
-                    answer=tempPalindrome;
-            }
-            
-            if(s.charAt(i)==s.charAt(i+1)){
-                leftPivot=i;
-                rightPivot=i+1;
-                String tempPalindrome="";
-                while(leftPivot>=0 && rightPivot<s.length() &&
-                    s.charAt(leftPivot)==s.charAt(rightPivot)){
-                    tempPalindrome = s.charAt(leftPivot--)
-                        + tempPalindrome
-                        + s.charAt(rightPivot++);
-                }
-                if(tempPalindrome.length()>answer.length())
-                    answer=tempPalindrome;
+            int oddLen = expandFromCenter(s, i, i);
+            int evenLen = expandFromCenter(s, i, i+1);
+            int len = Math.max(oddLen, evenLen);
+            if(end-start < len){
+                start = i - (len-1)/2;
+                end = i + len/2;
             }
         }
-        return answer;
+        
+        return s.substring(start, end+1);
+    }
+    
+    int expandFromCenter(String s, int L, int R){
+        while(L>=0 && R<s.length() &&
+                    s.charAt(L)==s.charAt(R)){
+            L--;
+            R++;
+        }
+        return R-L-1; //length of the palindrome
     }
 }
