61570fec53607cc76240bdb7fd4544432c0eee63
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-10-05 17:17:47 +0200

    Time: 0 ms (100.00%), Space: 41.1 MB (73.89%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -5,15 +5,12 @@ class Solution {
         if(nums.length==3) return Math.max(nums[0]+nums[2], nums[1]);
             
         
-        int[] maxs=new int[nums.length];
-        maxs[0]=nums[0];
-        maxs[1]=nums[1];
-        maxs[2]=nums[2]+nums[0];
+        nums[2]=nums[2]+nums[0];
         
-        for(int i=3; i< maxs.length; i++){
-            maxs[i]=nums[i]+Math.max(maxs[i-2], maxs[i-3]);
+        for(int i=3; i< nums.length; i++){
+            nums[i]=nums[i]+Math.max(nums[i-2], nums[i-3]);
         }
         
-        return Math.max(maxs[maxs.length-1], maxs[maxs.length-2]);
+        return Math.max(nums[nums.length-1], nums[nums.length-2]);
     }
 }
