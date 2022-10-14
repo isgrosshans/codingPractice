1f2ed7ef2839f82829f65fefbc66c8859db79380
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-10-14 14:16:54 +0200

    Time: 0 ms (100.00%), Space: 39.2 MB (88.22%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int answer=0;
+        int oneback=1;
+        int twoback=0;
+        for(int i=1; i<=n; i++){
+            answer=oneback+twoback;
+            twoback=oneback;
+            oneback=answer;
+        }
+        return answer;
+    }
+}
