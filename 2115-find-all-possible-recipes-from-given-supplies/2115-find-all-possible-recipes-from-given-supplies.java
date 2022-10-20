c2c846d12053e5d2c6731172613106755d5011ff
Isabella <44622575+isgrosshans@users.noreply.github.com>
2022-10-20 14:42:38 +0200

    Time: 2383 ms (5.04%), Space: 77.1 MB (47.81%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,97 @@
+class Solution {
+    Set<String> supplies = new HashSet<>();
+    Map<String, List<String>> recipes = new HashMap<>();
+    List<String> possibleRecipes = new ArrayList<>();
+    
+    public List<String> findAllRecipes(String[] recipesIn, 
+                                       List<List<String>> ingredientsIn, 
+                                       String[] suppliesIn) {
+        
+        for(String s : suppliesIn){
+            supplies.add(s);
+        }
+        
+        int i=0;
+        
+        for(List<String> ingredientList : ingredientsIn){
+            recipes.put(recipesIn[i++], ingredientList);
+        }
+        
+        for (String r : recipes.keySet()){
+            if (isPossible(r)) {
+                possibleRecipes.add(r);
+            }
+        }
+        
+        return possibleRecipes;
+    }
+    
+    private boolean isPossible(String recipe) {
+        if(supplies.containsAll(recipes.get(recipe))){
+            supplies.add(recipe);
+            return true;
+        }
+        
+        Stack<String> stack = new Stack<>();
+        
+        stack.push(recipe);
+        
+        while(!stack.isEmpty()) {
+            String curr = stack.pop();
+            
+            for (String ing : recipes.get(curr)){
+                if(stack.contains(ing)) return false;
+                if(recipes.containsKey(ing)) stack.push(ing);
+                else if(!supplies.contains(ing)) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+}
+
+
+
+/*
+*   bread
+*   |     \
+*   yeast  flour
+*
+*
+*   
+*
+*           sandwich
+*           |       \
+*       bread       meat
+*       |   \
+*   yeast   flour
+*   
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
